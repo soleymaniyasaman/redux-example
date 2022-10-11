1a9d21dd5b737fc4da4dd29870c7342b2a48f580
YasaminSoleimani <soleymani.yasaman@yahoo.com>
2022-10-12 00:15:39 +0300

    display products from redux store

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,34 @@
-import { NavLink } from 'react-router-dom'
-import '../styles/navbar.scss'
+import { useSelector } from "react-redux";
+import { NavLink } from "react-router-dom";
+import "../styles/navbar.scss";
 
 const Navbar = () => {
+  const { cartProductIds } = useSelector((state) => state.cart);
   return (
     <nav className="navbar">
       <NavLink to="/" end>
         <i className="bi bi-shop-window" />
       </NavLink>
 
-      <NavLink to="/cart" title="cart" className={({ isActive }) => `${isActive && 'selected'}`}>
+      <NavLink
+        to="/cart"
+        title="cart"
+        className={({ isActive }) => `${isActive && "selected"}`}
+      >
         <i className="bi bi-cart3" />
-        <sup className="cart-number">7</sup>
+        <sup className="cart-number">{cartProductIds.length}</sup>
       </NavLink>
 
-      <NavLink to="/" className={({ isActive }) => (isActive ? 'selected' : '')} title="products" end>
+      <NavLink
+        to="/"
+        className={({ isActive }) => (isActive ? "selected" : "")}
+        title="products"
+        end
+      >
         <i className="bi bi-grid" />
       </NavLink>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
